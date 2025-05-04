70ec3c9522d09f48f1d5ade8820684fa4919f495
Harsh Sharma <harshvs415@gmail.com>
2025-05-05 07:55:40 +0800

    Create file

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,14 @@
+# Last updated: 5/5/2025, 7:55:40 AM
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict1 = {}
+
+        for i in range(len(nums)):
+            comp = target - nums[i]
+
+            if comp in dict1:
+                return [dict1[comp], i]
+            else:
+                dict1[nums[i]] = i
+
+        return [-1, -1]
