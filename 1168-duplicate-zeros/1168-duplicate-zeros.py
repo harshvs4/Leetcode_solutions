a0a4d8041653aaaadc78b279864b73537cae72ce
Harsh Sharma <harshvs415@gmail.com>
2025-05-05 09:05:19 +0800

    Time: 0 ms (100%), Space: 18.2 MB (17.89%) - LeetHub

---
+++ 1168-duplicate-zeros/1168-duplicate-zeros.py
@@ -0,0 +1,11 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        zeros = arr.count(0)
+        n = len(arr)
+        for i in range(n-1, -1, -1):
+            if i + zeros < n:
+                arr[i + zeros] = arr[i]
+            if arr[i] == 0: 
+                zeros -= 1
+                if i + zeros < n:
+                    arr[i + zeros] = 0
