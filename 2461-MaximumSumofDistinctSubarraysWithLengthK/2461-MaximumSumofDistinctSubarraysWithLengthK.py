4e5b09251730e3a7acd7b39d8a8889880cc18144
Harsh Sharma <harshvs415@gmail.com>
2025-05-07 07:59:17 +0800

    Create file

---
+++ 2461-MaximumSumofDistinctSubarraysWithLengthK/2461-MaximumSumofDistinctSubarraysWithLengthK.py
@@ -0,0 +1,24 @@
+# Last updated: 5/7/2025, 7:59:17 AM
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        maxi = float('-inf')
+        left = 0
+        visited = set()
+        s = 0
+
+        for right in range(n):
+            while nums[right] in visited:
+                visited.remove(nums[left])
+                s-=nums[left]
+                left+=1
+            s+=nums[right]
+            visited.add(nums[right])
+
+            if right-left+1 == k:
+                maxi = max(maxi, s)
+                s-=nums[left]
+                visited.remove(nums[left])
+                left+=1
+
+        return maxi if maxi != float('-inf') else 0
